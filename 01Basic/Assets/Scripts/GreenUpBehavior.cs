c045d65fcd6d411f694be8c7bc3b29d9044c2e25
michzelinger <mzelin@uw.edu>
2021-04-11 19:08:22 -0700

    added stuff

--- 01Basic/Assets/Scripts/GreenUpBehavior.cs
+++ 01Basic/Assets/Scripts/GreenUpBehavior.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class GreenUpBehavior : MonoBehaviour
 {
-   public float speed = 10f;
+   public float speed = 20f;
+   public float acceleration = 100f;
+   public float maxSpeed = 60f;
    public float mHeroRotateSpeed = 100f / 2f;
+
+   public float shootingRate = 0.2f;
+   private float timeStamp;
 
    public bool mFollowMousePosition = true;
    // Start is called before the first frame update
@@ -35,6 +40,14 @@ public class GreenUpBehavior : MonoBehaviour
       {
          if (Input.GetKey(KeyCode.W))
          {
+            if(speed < 60f)
+            {
+               speed += Time.deltaTime * acceleration;
+            }
+            else if(speed > 60f)
+            {
+               speed = 60f;
+            }
             pos += ((speed * Time.smoothDeltaTime) * transform.up);
          }
 
@@ -53,12 +66,13 @@ public class GreenUpBehavior : MonoBehaviour
             transform.Rotate(transform.forward, mHeroRotateSpeed * Time.smoothDeltaTime);
          }
       }
-      if (Input.GetKeyDown(KeyCode.Space))
+      if (Time.time >= timeStamp && Input.GetKey(KeyCode.Space))
       {
           GameObject e = Instantiate(Resources.Load("Prefabs/Egg") as GameObject);
           e.transform.localPosition = transform.localPosition;
           e.transform.rotation = transform.rotation;
           Debug.Log("Spawn Eggs:" + e.transform.localPosition);
+          timeStamp = Time.time + shootingRate;
       }
       transform.position = pos;
    }
